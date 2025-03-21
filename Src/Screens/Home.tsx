c1d3b30c9b9b66c1e0f4d238a6e9408a2d6e86e1
Mahmutt <mahmutbaglama98@gmail.com>
2025-03-21 23:49:57 +0300

    düzenleme

--- Src/Screens/Home.tsx
+++ Src/Screens/Home.tsx
@@ -3,7 +3,7 @@ import { View, Text, ScrollView, StyleSheet } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 
 const Home = () => {
-  const workoutCategories = ["Strength", "Cardio", "Flexibility", "HIIT", "Yoga", "Pilates", "CrossFit"];
+  const workoutCategories = ["Strength", "Cardio", "Flexibility", "HIIT", "Yoga", "Pilates"];
 
   return (
     <View style={styles.container}>
